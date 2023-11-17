0a450b6704290fb69a302440a17c815966a627fb
Ivo Calado <ivo.calado@hover.to>
2023-11-17 01:43:20 -0300

    Increase machine type

--- environments/production/app01/main.tf
+++ environments/production/app01/main.tf
@@ -10,7 +10,7 @@ resource "google_service_account" "default" {
 
 resource "google_compute_instance" "default" {
   name         = "my-instance"
-  machine_type = "n2-standard-2"
+  machine_type = "c3-standard-8"
   zone         = "us-central1-a"
 
   tags = ["foo", "bar"]
